2bb81af26a9dc3656a922c7fa5256cbfecbc4193
Dhruv kumar <108050502+Dhruv-Kumar-au49@users.noreply.github.com>
2022-12-14 21:29:04 +0530

    Add files via upload

---
+++ bookLibrary/DbConfig.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose')
+let DBurl = "mongodb+srv://DhruvDC7:12345@cluster0.0gjglo9.mongodb.net/?retryWrites=true&w=majority"
+
+// initDB()
+
+async function initDB(){
+
+    try{
+
+        await mongoose.connect(DBurl,{dbName:'openlibrary'})
+console.log('DB is connected');
+
+    }catch(err){
+
+        console.log('error in connecting');
+    }
+
+
+}
+
+module.exports= {initDB}
