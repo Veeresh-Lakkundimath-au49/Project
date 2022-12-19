ec2a87041c42ebd55194936b1fc73c9860338778
Dhruv kumar <108050502+Dhruv-Kumar-au49@users.noreply.github.com>
2022-12-19 19:51:13 +0530

    Add files via upload
    final update

--- public/profile.js
+++ public/profile.js
@@ -1,7 +1,39 @@
-let {user}=require('../Controllers/authControllers');
-let profile=document.getElementById("prof")
-profile.innerHTML=`<img src="${user.image}" id="profPic"/>`
-let btn = document.getElementById('library')
-btn.addEventListener('click',()=>{
-    console.log('one');
-})
+ 
+//let submitBtnEle=document.getElementById("submitBtn")
+//submitBtnEle.addEventListener('click',async ()=>{
+    let profileCall=async()=>{   
+        let result=await fetch('/profileData')
+        let receivedData=await result.json()
+        let profile=document.getElementById("profile")
+        let name=document.getElementById("name")
+        let email=document.getElementById("email")
+        let number = document.getElementById("number")
+        profile.innerHTML=`<img src="${receivedData.image}"/>`
+    name.innerText=`${receivedData.name}`
+    email.innerText=`${receivedData.email}`
+    number.innerHTML=`${receivedData.number}`
+    }
+    profileCall()
+    
+    
+    let librarybtn = document.querySelector('#library')
+    librarybtn.addEventListener('click',library)
+    
+    
+    function library(){
+        location.replace('/library')
+    } 
+
+//     let signupbtn = document.querySelector('.signup')
+// signupbtn.addEventListener('click',signup)
+
+
+// function signup(){
+//     location.replace('/signup')
+// } 
+       let logoutbtn = document.querySelector('.logout')
+       logoutbtn.addEventListener('click',logout)
+       
+function logout(){
+location.replace('/logout')
+}
