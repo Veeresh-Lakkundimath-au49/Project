975c6531f8515d82d77c98922c2d525400ce4273
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2022-12-21 22:50:18 +0530

    Delete Book heaven directory

--- Book heaven/public/login.js
+++
@@ -1,5 +0,0 @@
-const spanElement = document.querySelector('.singup-link');
-
-spanElement.addEventListener('click', ()=> {
-    location.replace('/signup');
-});
--- Book heaven/public/signup.js
+++
@@ -1,5 +0,0 @@
-const spanElement = document.querySelector('.click-here');
-
-spanElement.addEventListener('click', ()=> {
-    location.replace('/login');
-});
